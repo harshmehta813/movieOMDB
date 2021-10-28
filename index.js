3fe4f2f37fc5da4deed1b264d78c553f8b8ea9a3
Harsh Mehta <harshmehta813@gmail.com>
2021-10-28 23:57:05 +0530

    Movie Search Engine - 1

---
+++ index.js
@@ -0,0 +1,103 @@
+let query = "";
+
+function fetchMovieDetails(movieName){
+    return fetch(`https://www.omdbapi.com/?apikey=c75d84ce&t=${movieName}&type=movie&page=1`)
+        .then( ( res ) => {
+            return res.json()
+        })
+        .catch( ( err) => {
+            console.log(err);
+        });
+}
+
+function displayMovie(movieDetails){
+    const container = document.getElementById("movie");
+    container.innerHTML = null;
+
+    const info = document.createElement('div');
+    const movie = document.createElement('div');
+    const header = document.createElement('div');
+    
+    header.id = "movie-header";
+    movie.id = "movie-container";
+    info.id = "movie-info";
+
+    const poster = document.createElement('img');
+    const title = document.createElement('h1');
+    const releaseYear = document.createElement('h1');
+    const description = document.createElement('p');
+    const rating = document.createElement('h4');
+    const genre = document.createElement('p');
+    const director = document.createElement('p');
+    const actors  = document.createElement('p');
+    
+    poster.src = movieDetails.Poster
+    title.textContent = movieDetails.Title;
+    releaseYear.textContent = movieDetails.Year;
+    description.textContent = movieDetails.Plot;
+    genre.textContent = "Genres - " + movieDetails.Genre;
+    director.textContent = "Director - " + movieDetails.Director;
+    actors.textContent = "Actors - " + movieDetails.Actors;
+    rating.textContent = "IMDB Rating - " + movieDetails.imdbRating; 
+
+    
+
+    header.append( title, releaseYear )
+    info.append( header, description, genre, actors, director, rating)
+    movie.append( poster, info );
+    container.append(movie);
+}
+
+
+
+function validateInput(text){
+    if ( !text || text.length < 3 ){
+        return false;
+    }
+    return true;
+}
+
+function setSpinner(isLoading){
+    
+    const spinner = document.getElementById("spinner");
+    if ( isLoading ){
+        const container = document.getElementById("movie");
+        container.innerHTML = null;
+
+        spinner.className = "visible";
+    } else {
+        spinner.className = "hidden";
+    }
+}
+
+
+
+async function handleSearch(e){
+    try {
+        e.preventDefault();
+        setSpinner(true);
+        const searchQuery = document.getElementById("name");
+        const query = searchQuery.value;
+        
+        if ( !validateInput(query) ){
+            
+            setSpinner(false);
+            alert('Please enter the exact movie name')
+            return;
+        }
+
+        const movieResult = await fetchMovieDetails(query);
+        displayMovie(movieResult);
+        
+        setSpinner(false);
+
+        searchQuery.value = "";
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+window.addEventListener("load", ( ) => {
+    const btn = document.getElementById('search');
+    btn.addEventListener("click", handleSearch);
+})
